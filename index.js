7deb4aaf3c9f1a12dac3e715f8ea8eff406d5e8c
rakaplay <goldermini123@gmail.com>
2025-02-25 19:19:53 +0700

    Added functionality of inverting X

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+let flipX = true;
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('h1[id], h2[id], h3[id]').forEach(heading => {
     if (!heading.querySelector('.anch')) {
@@ -140,11 +140,12 @@ hands.onResults((results) => {
     const bezymFinger = landmarks[16];
     const mainPoint = landmarks[1];
     const mizinFinger = landmarks[20];
-
-    const relativeXClick = mainPoint.x;
-
-    
-    
+    let relativeXClick = 1 - mainPoint.x;
+    if (document.getElementById("flipX").selected){
+     relativeXClick = 1 - mainPoint.x;
+    } else {
+     relativeXClick = mainPoint.x;
+    }
     const relativeYClick = mainPoint.y;
     const absoluteXClick = relativeXClick * window.innerWidth;
     const absoluteYClick = relativeYClick * window.innerHeight;
